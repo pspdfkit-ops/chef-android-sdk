b27b6b93d57747cfa3350c57c1b9c8adabe4f4d9
Roland Moriz <roland@moriz.de>
2014-10-24 15:26:34 +0200

    idempotency hack

--- recipes/_components.rb
+++ recipes/_components.rb
@@ -3,11 +3,21 @@ setup_root       = node['android-sdk']['setup_root'].to_s.empty? ? node['ark']['
 android_home     = File.join(setup_root, node['android-sdk']['name'])
 android_bin      = File.join(android_home, 'tools', 'android')
 
+
+
+directory 'components cache directory' do
+  path lazy { ::File.join(Chef::Config[:file_cache_path], 'android-sdk', node['android-sdk']['version']) }
+  recursive true
+end
+
 # With "--filter node['android-sdk']['components'].join(,)" pattern,
 # some system-images were not installed as expected.
 # The easiest way I could find to fix this problem consists
 # in executing a dedicated 'android sdk update' command for each component to be installed.
 node['android-sdk']['components'].each do |sdk_component|
+
+  idempotence_cache_file = ::File.join(Chef::Config[:file_cache_path], 'android-sdk', node['android-sdk']['version'], sdk_component)
+
   script "Install Android SDK platforms and tools #{sdk_component}" do
     interpreter 'expect'
     environment(
@@ -36,6 +46,11 @@ node['android-sdk']['components'].each do |sdk_component|
         eof
       }
     EOF
-    not_if { File.exist?("#{setup_root}/#{node['android-sdk']['name']}-#{node['android-sdk']['version']}/temp") }
+    not_if { File.exist?(idempotence_cache_file) }
+  end
+
+  file idempotence_cache_file do
+    content ""
+    action :create
   end
 end
